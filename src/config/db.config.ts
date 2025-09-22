ca9a6b2ad63c89641e2db5572a28b6250c4d25c0
iamhasanhumane <hassandemon12@gmail.com>
2025-09-22 17:32:04 +0530

    Fixed SSL Config

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -4,10 +4,20 @@ import path from "path";
 
 dotenv.config({ path: path.resolve(__dirname, "../../.env.local") });
 
+const getSslConfig = () => {
+  if (process.env.NODE_ENV === "production") {
+    return {
+      require: true,
+      rejectUnauthorized: false, // Or true, depending on CA setup
+    };
+  }
+  return false; // Allow disabling for local development
+};
+
 export const sequelize = new Sequelize(process.env.DATABASE_URL as string, {
   dialect: "postgres",
   logging: false,
   dialectOptions: {
-    ssl: false,
+    ssl: getSslConfig(),
   },
 });
