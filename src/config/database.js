20febb81eced9721515ed9b4ec468b47e1c7a041
iamhasanhumane <hassandemon12@gmail.com>
2025-11-23 23:03:19 +0530

    Removed pg

--- src/config/database.js
+++ src/config/database.js
@@ -23,7 +23,7 @@ module.exports = {
   production: {
     use_env_variable: "DATABASE_URL",
     dialect: "postgres",
-    dialectModule: "pg",
+    // dialectModule: "pg",
     logging: false,
     dialectOptions: {
       ssl: {
